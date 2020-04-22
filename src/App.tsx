e79a3b4c490b79b6b61b990e8bf0b167e365f774
Furkan Tanyol <furkantanyol@gmail.com>
2020-04-22 17:32:47 +1000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import PlatformerGame from "./platformer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="retro-header">SpaceOcean</h1>
+      <PlatformerGame />
+    </div>
+  );
+}
+
+export default App;
