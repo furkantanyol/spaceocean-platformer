9b0a1ff6f52863adfe2a194ffae2e47776aef9e4
Furkan Tanyol <furkantanyol@gmail.com>
2020-04-22 17:35:48 +1000

    Updates

---
+++ precache-manifest.8af6321caea951f8c28d4dbf125cb813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91a5f259b9e167bd3e8538827c451ad",
+    "url": "/spaceocean-platformer/index.html"
+  },
+  {
+    "revision": "e953b147c1fb7f7f46e6",
+    "url": "/spaceocean-platformer/static/css/main.db7e5d40.chunk.css"
+  },
+  {
+    "revision": "e6d763c8eee09161a5b1",
+    "url": "/spaceocean-platformer/static/js/2.a16ea861.chunk.js"
+  },
+  {
+    "revision": "0e84afde8a138d28ddc413c2c1367a0a",
+    "url": "/spaceocean-platformer/static/js/2.a16ea861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e953b147c1fb7f7f46e6",
+    "url": "/spaceocean-platformer/static/js/main.5816ba79.chunk.js"
+  },
+  {
+    "revision": "8752e78c0c2cfcbcbc68",
+    "url": "/spaceocean-platformer/static/js/runtime-main.5aa729aa.js"
+  }
+]);
